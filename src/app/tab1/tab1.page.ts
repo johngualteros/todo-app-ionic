2d0fd8d93eccf9e1b7f9dd5bfe22b2fd31238f6b
John Gualteros <gualterosjohn40@gmail.com>
2022-10-01 20:49:45 -0500

    Final commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Task } from '../task';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  newTask: string;
+  tasks: Task[] = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
   constructor() {}
 
+  addTask(){
+    this.tasks.push({
+      id: this.newTask.length + 1,
+      title: this.newTask,
+      done: false
+    });
+    this.newTask = '';
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  toggleDone(id: number){
+    this.tasks.map(task => {
+      if(task.id === id){
+        task.done = !task.done;
+      }
+    });
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
